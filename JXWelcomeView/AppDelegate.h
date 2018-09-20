a010998e68bd7759cf972c6ffcf34c732fe6d93f
Jiles <632036801@qq.com>
2018-09-20 11:21:32 +0800

    Initial Commit

---
+++ JXWelcomeView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXWelcomeView
+//
+//  Created by 徐沈俊杰 on 2018/9/20.
+//  Copyright © 2018年 JX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JXWelcomeView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXWelcomeView
+//
+//  Created by 徐沈俊杰 on 2018/9/20.
+//  Copyright © 2018年 JX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
